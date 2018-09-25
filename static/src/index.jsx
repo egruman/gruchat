929a9a372d033b3391d508ad6679afde6d9fcaaf
U-COMPUTER\Ethan <ethan2065@gmail.com>
2018-09-24 20:23:20 -0400

    frontend listens to correct port

--- static/src/index.jsx
+++ static/src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as $ from 'jquery';
 import App from './App.jsx'
-import openSocket from 'socket.io-client';
 import 'bootstrap';
+import openSocket from 'socket.io-client';
 
 $(document).ready(()=>{
 fetch('/getport').then(res => res.json()).then(function (res) {
-		var host='//'+document.domain+':'+res.port;
+		var host='http://'+document.domain+':'+res.port;
 		const socket = openSocket(host);
-		console.log("port:"+res.port+", domain:"+document.domain);
+		console.log("socket: "+socket);
 		ReactDOM.render(<App socket={socket} />, 
 			document.getElementById('root'));
 	});
