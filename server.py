77584045a1e58dcca90c396de3c412b8f38eaf56
U-COMPUTER\Ethan <ethan2065@gmail.com>
2018-09-28 20:12:42 -0400

    debugging

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 from flask import Flask, render_template, request, make_response, jsonify, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
@@ -7,8 +8,10 @@ app = Flask(__name__)
 
 app.debug = False
 key= "123"
-# if ('SECRET_KEY' in os.environ):
-#   key = os.environ['SECRET_KEY']
+if ('SECRET_KEY' in os.environ):
+    key = os.environ['SECRET_KEY']
+# else:
+#     sys.exit()
 
 app.config['SECRET_KEY'] = key
 socketio = SocketIO(app)
@@ -22,19 +25,19 @@ def test_connect():
     print('Connected')
 
 @socketio.on('login', namespace='/chat')
-def send_back(msg):
+def send_back_login(msg):
     emit('login response', msg, broadcast=True)
 
 @socketio.on('logout', namespace='/chat')
-def send_back(msg):
+def send_back_logout(msg):
     emit('logout response', msg, broadcast=True)
 
 @socketio.on('welcome', namespace='/chat')
-def send_back(msg): 
+def send_back_welcome(msg): 
     emit('welcome response', msg, broadcast=True)
 
 @socketio.on('send message', namespace='/chat')
-def send_back(msg): 
+def send_back_message(msg): 
     emit('recieve message', msg, broadcast=True)
 
 if __name__ == '__main__':
